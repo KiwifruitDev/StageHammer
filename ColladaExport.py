c282d958aa8afaf8887c47c2977d5349771de01e
KiwifruitDev <browncarlos@protonmail.com>
2023-09-02 12:51:44 -0700

    v1.0.1

--- ColladaExport.py
+++ ColladaExport.py
@@ -4,8 +4,8 @@
 import bpy
 import sys
 import os
-from io_import_vmf import import_vmf
-from vmfpy.fs import VMFFileSystem
+from plumber.plumber import Importer, FileSystem
+from plumber.asset import AssetCallbacks
 
 # Get the arguments
 argv = sys.argv
@@ -31,42 +31,119 @@ if bpy.data.objects.get("Camera") is not None:
     bpy.ops.object.delete()
 
 # Import the VMF
-data_dirs = [
-    'C:\Program Files (x86)\Steam\steamapps\common\Source SDK Base 2013 Multiplayer\hl2',
-    'C:\Program Files (x86)\Steam\steamapps\common\Source SDK Base 2013 Multiplayer\hl2\materials',
-]
-data_paks = []
+rootgame = "D:\SteamLibrary\steamapps\common\Source SDK Base 2013 Multiplayer"
+game = "hl2"
 
-fs = VMFFileSystem(data_dirs, data_paks, index_files=True)
+fs = FileSystem("HALF-LIFE 2", [
+    ("WILDCARD", rootgame + "/" + game + "/custom"),
+    ("VPK", rootgame + "/" + game + "/hl2_pak.vpk"),
+    ("VPK", rootgame + "/" + game + "/hl2_textures.vpk"),
+    ("VPK", rootgame + "/" + game + "/hl2_misc.vpk"),
+    ("DIR", rootgame + "/" + game),
+])
 
-importer = import_vmf.VMFImporter(  fs, None,
-                                    import_solids=True, import_overlays=False,
-                                    import_props=False, optimize_props=False,
-                                    skip_collision=True, skip_lod=False,
-                                    import_sky_origin=False,
-                                    import_sky=False, sky_resolution=512,
-                                    import_materials=True,
-                                    simple_materials=True, cull_materials=True,
-                                    texture_interpolation='Linear',
-                                    editor_materials=False,
-                                    reuse_old_materials=False,
-                                    reuse_old_models=False,
-                                    import_lights=False,
-                                    scale=1.0, epsilon=0.0001,
-                                    light_factor=1.0, sun_factor=1.0,
-                                    ambient_factor=1.0,
-                                    verbose=False,
-                                    skip_tools=False,
-                                    separate_tools=False,
-                                    blend_use_vertex_alpha=False)
+asset_callbacks = AssetCallbacks(
+    bpy.context,
+    main_collection=None,
+    brush_collection=None,
+    overlay_collection=None,
+    prop_collection=None,
+    light_collection=None,
+    entity_collection=None,
+    apply_armatures=False,
+)
 
-with importer:
-    importer.load(inputPath, bpy.context, None)
+try:
+    importer = Importer(
+        fs,
+        asset_callbacks,
+        3,
+        import_lights=False,
+        light_factor=0,
+        sun_factor=0,
+        ambient_factor=0,
+        import_sky_camera=False,
+        sky_equi_height=None,
+        import_unknown_entities=False,
+        scale=1,
+        target_fps=24,
+        remove_animations=True,
+        simple_materials=True,
+        allow_culling=True,
+        editor_materials=False,
+        texture_interpolation="Linear",
+        # automatic map data path detection happens here
+        vmf_path=inputPath,
+        map_data_path="",
+    )
+except OSError as err:
+    print(f"Could not parse vmf: {err}")
+
+try:
+    importer.import_vmf(
+        inputPath,
+        False,
+        import_brushes=True,
+        import_overlays=True,
+        epsilon=0.001,
+        cut_threshold=0.001,
+        merge_solids="SEPARATE",
+        invisible_solids="SKIP",
+        import_materials=True,
+        import_props=True,
+        import_entities=False,
+        import_sky=False,
+        scale=1,
+    )
+except OSError as err:
+    print(f"Could not import vmf: {err}")
+
+asset_callbacks.finish()
+
+# Select all objects
+bpy.ops.object.select_all(action='SELECT')
+
+# Remove all armature modifiers
+for obj in bpy.context.selected_objects:
+    for modifier in obj.modifiers:
+        if modifier.type == "ARMATURE":
+            obj.modifiers.remove(modifier)
+    # Apply position/rotation to geometry
+    bpy.context.view_layer.objects.active = obj
+    bpy.ops.object.transform_apply(location=True, rotation=True, scale=True)
+
+# Remove all armatures
+for armature in bpy.data.armatures:
+    bpy.data.armatures.remove(armature)
+
+# Rename all material slot names without prefixed path
+for material in bpy.data.materials:
+    # Get last slash index
+    lastSlash = material.name.rfind("/")
+    # Get the material name
+    materialName = material.name[lastSlash + 1:]
+    # Set the material name
+    material.name = materialName
+
+# Rename all image names without prefixed path
+for image in bpy.data.images:
+    # Ignore Render Result and Viewer Node
+    if image.name == "Render Result" or image.name == "Viewer Node":
+        continue
+    # Get last slash index
+    lastSlash = image.name.rfind("/")
+    # Get the image name
+    imageName = image.name[lastSlash + 1:]
+    # Set the image name
+    image.name = imageName
 
 # Save as DAE (Y up, -Z forward)
-bpy.ops.wm.collada_export(filepath=outputPath, check_existing=False, export_global_forward_selection='-Z', export_global_up_selection='Y', apply_global_orientation=True)
+bpy.ops.wm.collada_export(filepath=outputPath,
+    check_existing=False,
+    export_global_forward_selection='-Z',
+    export_global_up_selection='Y',
+    apply_global_orientation=True,
+)
 
 # Exit Blender
 bpy.ops.wm.quit_blender()
-
-                                  
